e96e401643469b35f7ddaa3c90eae11ac2d8771d
david-peyre <david.peyre@laplateforme.io>
2023-11-20 17:36:54 +0100

    ajout du pour aller plus loin

---
+++ jour01/papl/main.py
@@ -0,0 +1,8 @@
+chaine = input("Entrer votre chaine de caractères")
+nb_e = chaine.count("e")
+if "e" in chaine:
+    print("Il y a", nb_e, "e dans votre chaine de carractère")
+else:
+    print("Il n'y a pas de e dans cette chaine de carractère")  
+
+
