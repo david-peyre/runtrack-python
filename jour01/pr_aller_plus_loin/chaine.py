c8bf8999651330de849d3fc3854b2ed103f68da8
david-peyre <david.peyre@laplateforme.io>
2023-11-21 00:38:56 +0100

    modifs mineures

--- jour01/pr_aller_plus_loin/chaine.py
+++ jour01/pr_aller_plus_loin/chaine.py
@@ -2,8 +2,8 @@ chaine = input("Entrer votre chaine de caractères")
 nb_e = chaine.count("e")
 
 if "e" in chaine:
-    print("Il y a", nb_e, "e dans votre chaine de carractères")
+    print("Il y a", nb_e, "e dans votre chaine de caractères")
 else:
-    print("Il n'y a aucun e dans cette chaine de carractères")  
+    print("Il n'y a aucun e dans cette chaine de caractères")  
 
 
