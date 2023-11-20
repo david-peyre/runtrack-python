8969ecf84cc998a4f6e4df82796e7166242e68e8
david-peyre <david.peyre@laplateforme.io>
2023-11-21 00:37:14 +0100

    modifs mineures

--- jour01/papl/main.py
+++
@@ -1,8 +0,0 @@
-chaine = input("Entrer votre chaine de caractères")
-nb_e = chaine.count("e")
-if "e" in chaine:
-    print("Il y a", nb_e, "e dans votre chaine de carractère")
-else:
-    print("Il n'y a pas de e dans cette chaine de carractère")  
-
-
---
+++ jour01/pr_aller_plus_loin/chaine.py
@@ -0,0 +1,8 @@
+chaine = input("Entrer votre chaine de caractères")
+nb_e = chaine.count("e")
+if "e" in chaine:
+    print("Il y a", nb_e, "e dans votre chaine de carractères")
+else:
+    print("Il n'y a aucun e dans cette chaine de carractères")  
+
+
