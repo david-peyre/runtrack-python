3065adadd83da22058a8049f1584fcf6ef72bbcd
david-peyre <david.peyre@laplateforme.io>
2023-11-21 14:35:02 +0100

    amélioration job07

--- jour02/job07/main.py
+++ jour02/job07/main.py
@@ -2,10 +2,12 @@ chaine = "abcdefghijklmnopqrstuvwxyz" * 10
 
 longueur_chaine = len(chaine)
 niveau = 0
+caracteres_par_niveau = 1
 
-while niveau * (niveau + 1) // 2 < longueur_chaine:
-    debut = niveau * (niveau + 1) // 2
-    fin = (niveau + 1) * (niveau + 2) // 2
+while niveau * caracteres_par_niveau < longueur_chaine:
+    debut = (niveau * (niveau + 1) // 2) * 2  # Ajustement ici pour obtenir les indices corrects
+    fin = ((niveau + 1) * (niveau + 2) // 2) * 2  # Ajustement ici également
     ligne = chaine[debut:fin]
-    print(ligne.center(50))  # Ajuste la largeur de l'affichage
+    print(ligne.ljust(50))
     niveau += 1
+    caracteres_par_niveau += 2
