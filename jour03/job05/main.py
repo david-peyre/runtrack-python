a6d5d353093c99f50978ba08dc5fece1cd6f56d2
david-peyre <david.peyre@laplateforme.io>
2023-11-22 15:51:02 +0100

    simplification code job05

--- jour03/job05/main.py
+++ jour03/job05/main.py
@@ -1,20 +1,19 @@
-num1 = int(input("Entre le premier terme de l'opération (nombre)"))
+num1 = float(input("Entre le premier terme de l'opération (nombre)"))
 operator = input("Entre le second terme de l'opération (opérateur)")
-num2 = int(input("Entre le troisième terme de l'opération (nombre)"))
+num2 = float(input("Entre le troisième terme de l'opération (nombre)"))
 
 def calcule(num1, operator, num2):
-    if operator == "+":
-        return (num1 + num2)
-    elif operator == "-":
-        return (num1 - num2)
-    elif operator == "/":
-        if num2 == 0:
-            return "La division par zéro n'est pas définie en mathématiques"
-        else:
-            return (num1 / num2)
-    elif operator == "*":
-        return (num1 * num2)
-    elif operator == "%":
-        return (num1 % num2)
+    match operator:
+        case "+":
+            return num1 + num2
+        case "-":
+            return num1 - num2
+        case "/":
+            return "La division par zéro n'est pas définie en mathématiques" if num2 == 0 else num1 / num2
+        case "*":
+            return num1 * num2
+        case "%":
+            return num1 % num2
 
-print(f"Résultat de {num1} {operator} {num2} = {calcule(num1, operator, num2)}")
+resultat = calcule(num1, operator, num2)
+print(f"Résultat de {num1} {operator} {num2} = {resultat}")
