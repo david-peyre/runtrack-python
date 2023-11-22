fe6c676991800a9ba788b9b30830dda16ab1324e
david-peyre <david.peyre@laplateforme.io>
2023-11-22 15:32:15 +0100

    job07 ok

---
+++ jour03/job07/main.py
@@ -0,0 +1,17 @@
+def fonction(language):
+    if language == "JavaScript":
+        print("tu es un développeur web")
+    elif language == "python":
+        print("tu es un développeur IA")
+    elif language == "java":
+        print("tu es un développeur logiciel")
+    elif language == "reactjs":
+        print("tu es un développeur mobile")
+    else:
+        print("un jour, je serai le meilleur développeur...")
+    
+fonction("JavaScript")
+fonction("python")
+fonction("java")
+fonction("reactjs")
+fonction("SpongeBob")
