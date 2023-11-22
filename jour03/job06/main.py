15a07e7d663fca3201a0df2a613ae34dbb467613
david-peyre <david.peyre@laplateforme.io>
2023-11-22 15:12:43 +0100

    job06 ok

---
+++ jour03/job06/main.py
@@ -0,0 +1,11 @@
+def fonction(nombre):
+    if nombre > 0:
+        print("positif")
+    elif nombre < 0:
+        print("négatif")
+    elif nombre == 0:
+        print("nul")
+    
+fonction(-1)
+fonction(1)
+fonction(0)
